8d5b437a7b3de0c057b6da163f1fc49c329a3ce7
asem <asooome944@gmail.com>
2020-05-20 13:23:20 +0300

    hello world and multi line new line

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print('hello')
+#this is heelo world application
+print('hello world!\n')
+
+print("Hello world! 2")
+#this is multi line
+print(''' this is multiple line code Hello world
+today is very sunny''')
