21c528c9d51cb1808d13570869f572b197ba3d7e
asem <asooome944@gmail.com>
2020-05-27 22:27:48 +0300

    opretors

---
+++ opretors.py
@@ -0,0 +1,25 @@
+age = 11
+age1 = 6
+print(age > age1)
+print(age < age1)
+print(age == age1)
+print(2 == 2)
+print(age != age1)
+print(age >= age1)
+print(age <= age1)
+
+
+
+print(True and True)
+print(True and False)
+print(False and False)
+
+
+
+print(True or True)
+print(True or False)
+print(False or False)
+
+
+print(not True)
+print(not False)
