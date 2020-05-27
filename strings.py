e7418901955800e201e2eda2e3a2616d20491228
asem <asooome944@gmail.com>
2020-05-27 18:14:11 +0300

    string

---
+++ strings.py
@@ -0,0 +1,13 @@
+name = 'asem'
+lname = 'asem'
+full_name ='asem fahad'
+age = 11
+a= 65
+value = 3
+boolean = True
+print (name.upper() + " " + lname.capitalize())
+print(full_name.replace('fahad','alarfj'))
+#print (age + " " + value)
+#a = '*'*5800
+#print(a)
+print(a+age)
