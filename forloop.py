93e210f0a02cd5b576edeb4122e2d04caa8153e1
asem <asooome944@gmail.com>
2020-06-04 02:45:55 +0300

    functions

--- forloop.py
+++ forloop.py
@@ -14,3 +14,4 @@ print(subjects[-1])
 
 for x in subjects:
     print(x)
+    
---
+++ fuctions.py
@@ -0,0 +1,63 @@
+def liner():
+    print('------------------')
+
+def add (num1,num2):
+    print(int(num1)+int(num2))
+
+n1= input('enter first num : ')
+n2= input('enter sec num : ')
+add(n1,n2)
+
+def sub (num1,num2):
+    print(int(num1)-int(num2))
+
+sub(n1,n2)
+
+
+def mul (num1,num2):
+    print(int(num1)*int(num2))
+
+mul(n1,n2)
+
+def div (num1,num2):
+    print(int(num1)/int(num2))
+
+
+div(n1,n2)
+
+
+def power (num1,num2):
+    print(int(num1)**int(num2))
+
+power(n1,n2)
+
+
+
+
+
+
+
+#print('asem fahad')
+#liner()
+#print(22)
+#liner()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
