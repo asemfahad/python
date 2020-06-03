d0bf39a9f99a57f223053f9827efe08513e22a22
asem <asooome944@gmail.com>
2020-06-04 02:35:35 +0300

    dictionries

---
+++ dictionries.py
@@ -0,0 +1,8 @@
+subjects_with_student = {"arabic":3, " EN":3, "Math":2}
+print(subjects_with_student)
+students = {"Arabic":["ali","mohammed","mona"],"En":["jack","ahmed","john"]}
+print(students)
+students['Math']=["salah","hussen"]
+print(students)
+students.update({"history":"asem"})
+print(students)
