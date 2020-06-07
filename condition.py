b47455669938403ca2e7abe38f9cc035f5f7a6bf
asem <asooome944@gmail.com>
2020-06-08 01:16:40 +0300

    more

--- condition.py
+++ condition.py
@@ -1,7 +1,33 @@
-username = input('please enter your username: ')
+"""username = input('please enter your username: ')
 passwword = input('please enter your password: ')
 
 if username == 'asem' and passwword == '0000' or username == 'fahad' or username == 'haifa':
     print('welcome')
 else:
     print('error password or username')
+
+
+x = int(input("please Enter A Number : "))
+
+if x == 100:
+    print('x = 100')
+    if x < 100:
+        print("x < 100")
+    elif x < 300:
+        print("x < 300")
+    
+elif x == 150 : print(" x = 150 ")
+elif x == 200:
+    print(" x = 200")
+else :
+    print(x)
+print('x=100')if x == 100 else print(" x = ", x)"""
+
+x = 3
+y = 4
+z = 5
+if all([x == 3 , y == 4, z == 5]):
+    print('all done')
+if any[x==9 , y == 7, z == 1]:
+    print('erroe has happend')
+    
--- while_loops.py
+++ while_loops.py
@@ -1,4 +1,4 @@
-num_of_triess = 0
+"""num_of_triess = 0
 while num_of_triess < 3 :
     username = input('please enter your user name : ')
     password = input('please enter your password : ')
@@ -11,3 +11,56 @@ while num_of_triess < 3 :
         print('error')
         num_of_triess +=1
         print('you have : ' + str( 3- num_of_triess ) + 'left')
+
+
+
+a = 0
+b = 0
+while a < 4:
+    a+=1
+    b=0
+    while b < 4 :
+        b+=1
+        print(a,b)
+    
+for a in range(1,5):
+    for b in range(1,5):
+        print(a,b)
+print(list(range(0,2000000+500000,5000)))
+
+
+for letter in "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhonhhhhhhhhhht":
+    if not(letter == "h"):
+        print(letter)
+y = 10 
+while y > 0:
+    y-=1
+    if y == 5:
+      continue
+    print(y)     
+print('bye')
+
+start = int(input('Enter the starting number : '))
+end = int(input('How High shoyld i go : '))
+print('number \t\t square')
+print('-------------------------')
+
+while start <= end:
+    square = start **2
+    print(start,"\t\t",square)
+    start+=1
+print('number \t\t square')
+print('-------------------------')
+for i in range(start,end+1):
+    square = i **2
+    print(i,'\t\t',square)"""
+size = int(input('please enter how many colms :'))
+for r in range(size):
+    for c in range((r+1)**2):
+        print('*', end = '')
+    print()    
+while size > 0:
+    for c in range(size**2):
+        print('*',end= '')
+    size-=1
+    print()
